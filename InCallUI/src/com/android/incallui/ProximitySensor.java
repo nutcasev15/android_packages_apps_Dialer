1b0dc2960b4b3a0d513cf6a45b0f1f0521a13068
sayeed99 <sayeed.afridi2009@gmail.com>
2016-12-25 21:10:43 +0530

    InCall : only enable proximity when in ongoing call not on in incoming call.
    Change-Id: Iadccb445f3ee25af2132b27c9c344042d21cf094

--- InCallUI/src/com/android/incallui/ProximitySensor.java
+++ InCallUI/src/com/android/incallui/ProximitySensor.java
@@ -279,8 +279,7 @@ public class ProximitySensor implements AccelerometerListener.OrientationListene
                     CMSettings.System.getInt(mContext.getContentResolver(),
                             CMSettings.System.PROXIMITY_ON_WAKE, mProximityWakeDefault) == 1;
 
-            if ((mIsPhoneOffhook || (mHasIncomingCall && proximityOnWake))
-                    && !screenOnImmediately) {
+            if ((mIsPhoneOffhook) && !screenOnImmediately) {
                 Log.d(this, "Turning on proximity sensor");
                 // Phone is idle.  We don't want any special proximity sensor
                 // behavior in this case.
